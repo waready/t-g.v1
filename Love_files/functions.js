a434da8562fc81b393c7e878b2d00169d2e31469
waready <elbarto119@hotmail.com>
2021-12-04 01:32:34 -0500

    modal v2

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -38,8 +38,12 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
+	
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	console.log(current);
+	console.log(date);
+	console.log(seconds);
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
@@ -55,6 +59,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "<p> <span class=\"digit\">" + days + "</span> (D) <span class=\"digit\">" + hours + "</span> (M) <span class=\"digit\">" + minutes + "</span> (H) <span class=\"digit\">" + seconds + "</span> (S)</p>" ; 
 	$("#clock").html(result);
 }
